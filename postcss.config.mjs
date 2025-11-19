be2756a822b875aaf8faa720042902cdc13b96a3
mohdajnas <mohdajnas@yahoo.com>
2025-11-19 13:29:54 +0530

    fix: update PostCSS config for Tailwind CSS v4

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
-export default config
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
